cb339641b6881b8728e30a2b5880f315e5fa14e5
asb01-er <ekelemernesto17@gmail.com>
2025-11-25 21:02:06 +0200

    first commit

---
+++ Chapter02/Reactapp/src/App.js
@@ -0,0 +1,34 @@
+import logo from './logo.svg';
+import './App.css';
+import Products from './Products';
+import { Component } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>
+        My First Ever React App!
+      </h1>
+      <Products />
+    </div>
+  );
+}
+
+// class App extends Component {
+//   formatName(user) {
+//     return user.firstName + ' ' + user.lastName;
+//   }
+//   render() {
+//     const user = {
+//       firstName: 'Ernest',
+//       lastName: 'Ekelem'
+//     };
+//     return (
+//       <div>
+//         <h1>Hello, {this.formatName(user)}</h1>
+//       </div>
+//     );
+//   }
+// }
+
+export default App;
